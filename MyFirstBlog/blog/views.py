af94b5437124d8c4986ee074a5390fba697167e6
KCY <cks402@naver.com>
2023-10-30 20:34:14 +0900

    오늘 마무리(로그인체크믹스인 오류 해결못함)

--- MyFirstBlog/blog/views.py
+++ MyFirstBlog/blog/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from typing import Any
 from django.db import models
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+# custom .py
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
@@ -14,9 +18,8 @@ from django.views.generic import (
     DetailView,
     CreateView,
 )
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class PostList(ListView):
@@ -42,10 +45,26 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return redirect("post_list")
 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(UserPassesTestMixin, UpdateView):
     model = Post
     fields = "__all__"
     template_name = "blog/post_form.html"
+
+    def test_func(self):
+        """
+        이 메서드에서 사용자의 권한을 확인
+        """
+        post = self.get_object()  # 업데이트할 게시물 가져오기
+        return self.request.user == post.author  # 사용자가 게시물의 작성자인지 확인
+
+    def handle_no_permission(self):
+        """
+        사용자가 권한이 없는 경우 처리할 동작 정의
+
+        예를 들어, 권한이 없을 때 리디렉션하거나 오류 메시지를 표시할 수 있음
+        """
+        messages.error(self.request, "글쓴이가 아닙니다.")
+        return super().handle_no_permission()
 
     def post(self, request, pk):
         form = PostForm(request.POST, request.FILES)
