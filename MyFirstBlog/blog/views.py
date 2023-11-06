0c6bed57802b007eb9fbe9d3eba7a2b85fa59706
KCY <cks402@naver.com>
2023-11-06 15:39:21 +0900

    전체 카테고리 for문으로 불러오기

--- MyFirstBlog/blog/views.py
+++ MyFirstBlog/blog/views.py
@@ -32,6 +32,11 @@ class PostList(ListView):
     context_object_name = "posts"
     paginate_by = pageslice
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
+
 
 class PostSearch(ListView):
     model = Post
--- MyFirstBlog/main/views.py
+++ MyFirstBlog/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
-from blog.models import Post
+from blog.models import Post, Tag
 
 # Create your views here.
 
@@ -12,6 +12,11 @@ class PostPick6(ListView):
 
     def get_queryset(self):
         return Post.objects.all().order_by("-pk")[:6]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
 
 
 index = PostPick6.as_view()
